0a18c1ca98fd62b477c61b532c77619172703231
Luong The Hai <hailuongthe2000@gmail.com>
2023-04-19 21:20:51 +0700

    Add color test

--- test/test.cpp
+++ test/test.cpp
@@ -21,3 +21,27 @@ TEST(EngineTest, TestCodeEqualToRaylibColor) {
   SekaiEngine::API::Color a(0x00e430ff);
   EXPECT_TRUE(a == (SekaiEngine::API::Color)GREEN);
 }
+
+TEST(EngineTest, TestCodeWithRed) {
+    SekaiEngine::API::Color a(0x00e430ff);
+    ::Color b = GREEN;
+    EXPECT_TRUE(a.r() == b.r);
+}
+
+TEST(EngineTest, TestCodeWithGreen) {
+    SekaiEngine::API::Color a(0x00e430ff);
+    ::Color b = GREEN;
+    EXPECT_TRUE(a.g() == b.g);
+}
+
+TEST(EngineTest, TestCodeWithBlue) {
+    SekaiEngine::API::Color a(0x00e430ff);
+    ::Color b = GREEN;
+    EXPECT_TRUE(a.b() == b.b);
+}
+
+TEST(EngineTest, TestCodeWithAlpha) {
+    SekaiEngine::API::Color a(0x00e430ff);
+    ::Color b = GREEN;
+    EXPECT_TRUE(a.a() == b.a);
+}
